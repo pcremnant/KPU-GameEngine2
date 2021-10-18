b4472b8a04d46414bb63bd918237380649148244
Park Chan <pcremnant@naver.com>
2021-10-18 20:51:16 +0900

    floor texture work save

--- GameEngine2/Assets/02.Scripts/MainGame/MainGame.cs
+++ GameEngine2/Assets/02.Scripts/MainGame/MainGame.cs
@@ -10,25 +10,23 @@ public class MainGame : MonoBehaviour
     // 임시 확인용 코드
     [SerializeField] StageSelectScene selectScene;
 
-    public GameScene curScene => _curScene;
-    private GameScene _curScene;
+    public GameScene gameScene => _scenes.Peek();
+    private Stack<GameScene> _scenes;
 
     private void Awake()
     {
-        // 스테이지 변경 테스트
-        // _curScene = new StageSelectScene();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        selectScene.Initialize();
-        // _curScene.Initialize();
+        _scenes.Push(selectScene);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // _curScene.
+
     }
 }
