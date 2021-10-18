7ca3f188eada683e12685e9b4ec0338423b7afc7
oychan <oyc0507@gmail.com>
2021-10-18 21:11:32 +0900

    플레이어 추적 AI(테스트)

---
+++ GameEngine2/Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform target;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = target.transform.position;
+    }
+}
