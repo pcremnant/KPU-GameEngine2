58fe6e3a8e6f5dad1ae1d7a09e697c990b375359
oychan <oyc0507@gmail.com>
2021-11-25 22:28:27 +0900

    적 스폰, 죽음, 강화형 추가

--- GameEngine2/Assets/02.Scripts/MainGame/Stage.cs
+++ GameEngine2/Assets/02.Scripts/MainGame/Stage.cs
@@ -5,14 +5,18 @@ public class Stage : MonoBehaviour
     public int stageNumber => _stageNumber;
     [SerializeField] private int _stageNumber;
 
+    public static bool SpawnerOn = false;
+
     public void OnStage()
     {
         gameObject.SetActive(true);
+        SpawnerOn = true;
     }
 
     public void OffStage()
     {
         gameObject.SetActive(false);
+        SpawnerOn = false;
     }
 
     public bool IsOnStage()
